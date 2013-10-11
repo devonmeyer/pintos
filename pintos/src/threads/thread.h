554e246f36c748600ae24907d746a7356687f15c
Ryan Cleary <rcleary@usc.edu>
2013-10-10 18:35:30 -0700

    Adding one file i missed.

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -95,7 +95,7 @@ struct thread
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
 
-    struct list *donated_priority; 
+    struct list donated_priority; 
 
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
