fdcfdefb047748a60f605c2a1f7e4c63bb916186
devonmeyer <devonmeyer212@gmail.com>
2013-10-29 21:58:57 -0700

    Added some stuff to syscall.h

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -13,8 +13,44 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f) 
 {
   printf ("system call!\n");
-  thread_exit ();
+  switch(f->esp){
+  	case SYS_HALT:
+  		print_okay();
+  		printf("AKLSJD"asdf);
+  		break;
+    case SYS_EXIT:
+    	break;
+    case SYS_EXEC:
+    	break;
+    case SYS_WAIT:
+    	break;
+    case SYS_CREATE:
+    	break;
+    case SYS_REMOVE:
+    	break;
+    case SYS_OPEN:
+    	break;
+    case SYS_FILESIZE:
+    	break;
+    case SYS_READ:
+    	break;
+    case SYS_WRITE:
+    	break;
+    case SYS_SEEK:
+    	break;
+    case SYS_TELL:
+    	break;
+    case SYS_CLOSE:
+    	break;
+    default:
+    	thread_exit();  
+  }
+}
+
+static void
+print_okay(void){
+	printf("Got a system call that I expected!\n");
 }
