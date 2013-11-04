e7d2b6c049e7b7d040b32c14bae34006858d2a91
Alex <alexyoung1992@gmail.com>
2013-11-03 22:16:05 -0800

    Added print statement

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -47,8 +47,8 @@ process_execute (const char *cmdline)
   const char *process_name = parse_process_name(fn_copy); 
 
   /* Create a new thread to execute FILE_NAME. */
-  // tid = thread_create (cmdline, PRI_DEFAULT, start_process, (void*)process_name);
-  tid = thread_create (cmdline, PRI_DEFAULT, start_process, fn_copy);
+  tid = thread_create (cmdline, PRI_DEFAULT, start_process, (void*)process_name);
+  // tid = thread_create (cmdline, PRI_DEFAULT, start_process, fn_copy);
   if (tid == TID_ERROR)
     palloc_free_page (fn_copy); 
   return tid;
@@ -85,6 +85,7 @@ parse_process_args(const char *cmdline, int *argc, char **argv)
   if(token != NULL) {
     for(*argc = 0; token != NULL; *argc++) {
       strlcpy(argv[*argc], token, sizeof(token));
+      printf("Argument: %s\n", argv[*argc]);
       token = strtok_r(NULL, delimiters, &save_ptr);
     }
   } else { // If there are no arguments
