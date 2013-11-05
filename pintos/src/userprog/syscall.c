8b3b0658e7108c9cfe0f47b4dafb99f5fcae0fa9
Alex <alexyoung1992@gmail.com>
2013-11-05 00:39:06 -0800

    setup infrastructure for exec system call

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -7,11 +7,14 @@
 #include "threads/pte.h"
 #include "userprog/pagedir.h"
 #include "userprog/process.h"
+#include "devices/shutdown.h"
+#include "process.h"
 
 static void syscall_handler (struct intr_frame *);
 static bool is_valid_memory_access(const void *vaddr);
 static void get_arguments(struct intr_frame *f, int num_args, int * arguments);
 
+static pid_t system_exec (const char *cmd_line);
 static void system_open(struct intr_frame *f);
 static void system_write(struct intr_frame *f, int * arguments);
 
@@ -33,6 +36,7 @@ syscall_handler (struct intr_frame *f)
   switch(*num){
   	case SYS_HALT:
   		print_okay();
+      shutdown_power_off();
   		thread_exit();
   		break;
     case SYS_EXIT:
@@ -42,6 +46,9 @@ syscall_handler (struct intr_frame *f)
       // Need to set f->eax to arguments[0]
     	break;
     case SYS_EXEC:
+      print_okay();
+      get_arguments(f, 1, arguments);
+      f->eax = system_exec((const char*)arguments[i]);
     	break;
     case SYS_WAIT:
     	break;
@@ -74,6 +81,13 @@ syscall_handler (struct intr_frame *f)
     	thread_exit();  
       break;
   }
+}
+
+static pid_t 
+system_exec (const char *cmd_line)
+{
+  // pid is mapped exactly to tid, process_execute returns tid
+  pid_t pid = (pid_t)process_execute(cmd_line);
 }
 
 void
