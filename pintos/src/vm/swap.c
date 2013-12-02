ae1b86562d0759f9d06e524070ba28002dd4162a
Alex <alexyoung1992@gmail.com>
2013-12-02 15:57:51 -0800

    finished inital implementation of swap table

--- pintos/src/vm/swap.c
+++ pintos/src/vm/swap.c
@@ -1,27 +1,49 @@
 #include "vm/swap.h"
 
+/* Initializes the swap table by obtaining the swap block on disk
+   and creating the bitmap 
+ */ 
 void 
 init_st(void) 
 {
-	list_init(&st_list);
 	lock_init(&st_lock);
 
 	swap_block = block_get_role(BLOCK_SWAP);
+	if(!swap_block) {
+		PANIC("Error obtaining swap block in swap table");
+	}
 	st_bitmap = bitmap_create(block_size(swap_block));
+	if(!st_bitmap) {
+		PANIC("Error creating bitmap in swap table");
+	}
 	bitmap_set_all(st_bitmap, SWAP_FREE);
 }
 
+/* SUMMARY: Swaps a given frame in memory into swap disk
+   INPUT: Frame number to be swapped out and stored in swap disk.
+
+   Checks if swap partition is not full, otherwise PANIC.
+   Finds an empty swap slot and updates the bitmap to indicate
+   it is now in use.
+   Finds the page associated with given frame and notifies
+   corresponding supplementary page table where the page is
+   being stored in swap disk.
+   Writes the frame to the swap disk.	
+ */
 void 
 swap_frame_out_st(void* frame_number)
 {
 	lock_acquire(&st_lock);
 
+	if(!swap_block || !st_bitmap) {
+		PANIC("Error, swap table was not properly initalized");
+	}
+
 	block_sector_t sector = bitmap_scan_and_flip(st_bitmap, BITMAP_START, SECTORS_PER_PAGE, SWAP_FREE);
 
-	struct st_entry *entry = malloc(sizeof(struct st_entry));
-	entry->frame_number = frame_number;
-	entry->thread = thread_current();
-	entry->sector = sector;
+	if(sector == BITMAP_ERROR) {
+		PANIC("Error, swap partition is full");
+	}
 
 	void* page_number = get_entry_ft(frame_number);
    	swap_page_out_spt(page_number, sector);
@@ -33,35 +55,80 @@ swap_frame_out_st(void* frame_number)
    		sector++;
    	}
 
-	list_push_back(&st_list, &entry->elem);
-
 	lock_release(&st_lock);
 }
 
-void 
+/* SUMMARY: Swaps a frame in swap disk into memory
+   INPUT: Initial sector where the frame to be swapped in is stored.
+
+   Checks if designated swap slot contains valid data, otherwise PANIC.
+   Updates the bitmap to indicate the swap slot located at the given
+   sector is now free.
+   Reads the frame from the swap disk.
+ */
+void
 swap_frame_in_st(block_sector_t sector)
 {
 	lock_acquire(&st_lock);
 
+	if(!swap_block || !st_bitmap) {
+		PANIC("Error, swap table was not properly initalized");
+	}
+
+	if(bitmap_test(st_bitmap, sector) == SWAP_FREE) {
+		PANIC("Error trying to swap in a free block");
+	}
+
+	bitmap_scan_and_flip(st_bitmap, sector, SECTORS_PER_PAGE, SWAP_NOT_FREE);
+
+	void* frame_number; // NEED TO ALLOCATE THIS FRAME
+
+	int i;
+	for(i = 0; i < SECTORS_PER_PAGE; i++) {
+		block_read(swap_block, sector, frame_number);
+		frame_number += BLOCK_SECTOR_SIZE;
+		sector++;
+	}
+
+	// swap_page_in_spt(page_number);
+
 	lock_release(&st_lock);
 }
 
+/* SUMMARY: Frees all frames stored in swap disk for the current thread
+   
+   Fetches the start sectors for each page associated with the current
+   thread from the thread's supplementary page table.
+   FOREACH swap slot, checks if swap slot is in use, otherwise PANIC;
+   	THEN free swap slot by updating the bitmap to indicate free status
+   	for corresponding sectors.
+ */
 void 
-free_all_swap_slots(struct thread* thread)
+free_all_swap_slots_for_current_thread_st(void)
 {
 	lock_acquire(&st_lock);
 
-	// struct list* sectors_list = get_all_swapped_out_sector_nums_spt();
+	if(!swap_block || !st_bitmap) {
+		PANIC("Error, swap table was not properly initalized");
+	}
 
-	// struct list_elem *e;
-	// for (e = list_begin (&sectors_list); e != list_end (&sectors_list); e = list_next (e))
-	// {
- //      struct page_num_item = list_entry (e, struct page_num_item, elem);
- //      if(t_exists->tid == t->tid){
- //        return true;
- //      }      
- //    }
+	struct list* sectors_list = get_all_swapped_out_sector_nums_spt();
+
+	struct list_elem *e;
+	for (e = list_begin (sectors_list); e != list_end (sectors_list); e = list_next (e)){
+		struct sector_item* entry = list_entry (e, struct sector_item, elem);
+
+		block_sector_t sector = entry->sector_num;
+		if(bitmap_test(st_bitmap, sector) == SWAP_FREE) {
+			PANIC("Error trying to swap in a free sector");
+		}
+
+		int i;
+		for(i = 0; i < SECTORS_PER_PAGE; i++) {
+			bitmap_flip(st_bitmap, sector);
+			sector++;
+		}     
+    }
 
 	lock_release(&st_lock);
 }
-
--- pintos/src/vm/swap.h
+++ pintos/src/vm/swap.h
@@ -1,39 +1,32 @@
 #ifndef VM_SWAP_H_
 #define VM_SWAP_H_
 
+#include <list.h>
+#include <bitmap.h>
 #include "devices/block.h"
 #include "threads/synch.h"
 #include "threads/thread.h"
 #include "threads/vaddr.h"
 #include "vm/frame.h"
 #include "vm/page.h"
-#include <bitmap.h>
 
 #define SWAP_FREE 0
 #define SWAP_NOT_FREE 1
 #define SECTORS_PER_PAGE (PGSIZE/BLOCK_SECTOR_SIZE)
 #define BITMAP_START 0
 
-struct list st_list;
-struct lock st_lock;
-struct block* swap_block;
-struct bitmap* st_bitmap;
-
-struct st_entry {
-	void* frame_number;
-	block_sector_t sector;
-	struct thread* thread;
-	struct list_elem elem;
-};
+struct lock st_lock;			// Lock to prevent multi-threaded access
+struct block * swap_block;		// Block device partitioned for swap
+struct bitmap * st_bitmap;		// One to one bitmap of sectors to keep track of swap slots
 
 void init_st(void);
 void swap_frame_out_st(void* frame_number);
 void swap_frame_in_st(block_sector_t sector);
-void free_all_swap_slots(struct thread* thread);
+void free_all_swap_slots_for_current_thread_st(void);
 
 /*
 
-Scenarios for swap table:
+SCENARIOS FOR SWAP TABLE:
 
 1. Eviction, given a frame, should store the frame in a swap slot
 	- INPUT: Frame, OUTPUT: void
