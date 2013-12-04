02aab4e5806f049fe13d020409c7d48f19e6b4bf
Alex <alexyoung1992@gmail.com>
2013-12-04 01:14:51 -0800

    changed type of mapid from int to mapid_t

--- pintos/src/vm/mmap.h
+++ pintos/src/vm/mmap.h
@@ -7,8 +7,8 @@
 #include "vm/page.h"
 
 void init_mmapt(void);
-void add_mmapt_entry(int mapid, struct spt_entry * spt_entry);
-struct list * get_mmapt_entries(int mapid);
-void remove_mmapt_entry(int mapid);
+void add_mmapt_entry(mapid_t mapid, struct spt_entry * spt_entry);
+struct list * get_mmapt_entries(mapid_t mapid);
+void remove_mmapt_entry(mapid_t mapid);
 
 #endif
